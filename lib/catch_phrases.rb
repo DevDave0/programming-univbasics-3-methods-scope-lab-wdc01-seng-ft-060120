8fdb0595c1f0923855dd992e034d57b7d037da33
David Chung <dbchung3@yahoo.com>
2020-04-12 02:09:49 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -7,3 +7,9 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link 
+  take = "It's Dangerous To Go Alone! Take This."
+  puts take 
+end 
+
